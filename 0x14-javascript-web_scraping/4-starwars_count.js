23b74f64806fc59d0ddc212e72dd01c34cee9d45
abdul1ah33 <study.abdullah2005@gmail.com>
2024-08-14 04:40:19 -0700

    d

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,29 +1,31 @@
 #!/usr/bin/node
 
-const request = require("request");
-const url = `https://swapi-api.alx-tools.com/api/films/`;
-const characterID = 18;
+const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
 let count = 0;
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const data = JSON.parse(body);
-        const films = data.results;
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const films = data.results;
 
-        for (let i = 0; i < films.length; i++) {
-            const film = films[i];
-            const characters = film.characters;
+    for (let i = 0; i < films.length; i++) {
+      const film = films[i];
+      const characters = film.characters;
 
-            for (let j = 0; j < characters.length; j++) {
-                const character = characters[j];
+      for (let j = 0; j < characters.length; j++) {
+        const character = characters[j];
 
-                if (character.includes(characterID)) {
-                    count += 1;
-                }
-            }
+        if (character.includes(characterId)) {
+          count += 1;
         }
-        console.log(count);
+      }
     }
+
+    console.log(count);
+  }
 });
+
